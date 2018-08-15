eb2767aa6bb05aa194a2892c1ed5f46e218af9bc
wangshengjin <934457719@qq.com>
2018-08-15 15:47:54 +0800

    修改极验证ios端文件结构

---
+++ src/ios/AppDelegate+NBGeetest.h
@@ -0,0 +1,13 @@
+//
+//  AppDelegate+NBGeetest.h
+//  delegateExtention
+//
+//  Created by wangshengjin on 18/8/15.
+//  Copyright (c) 2018年 NBGeetest. All rights reserved.
+//
+
+#import "AppDelegate.h"
+#import <GT3Captcha/GT3Captcha.h>
+
+@interface AppDelegate (NBGeetest) <GT3CaptchaManagerDelegate>
+@end
--- src/ios/AppDelegate+NBGeetest.m
+++ src/ios/AppDelegate+NBGeetest.m
@@ -1,44 +1,16 @@
-/*
- Licensed to the Apache Software Foundation (ASF) under one
- or more contributor license agreements.  See the NOTICE file
- distributed with this work for additional information
- regarding copyright ownership.  The ASF licenses this file
- to you under the Apache License, Version 2.0 (the
- "License"); you may not use this file except in compliance
- with the License.  You may obtain a copy of the License at
-
- http://www.apache.org/licenses/LICENSE-2.0
-
- Unless required by applicable law or agreed to in writing,
- software distributed under the License is distributed on an
- "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- KIND, either express or implied.  See the License for the
- specific language governing permissions and limitations
- under the License.
- */
-
 //
-//  AppDelegate.m
-//  HelloWorld
+//  AppDelegate+NBGeetest.m
+//  delegateExtention
 //
-//  Created by ___FULLUSERNAME___ on ___DATE___.
-//  Copyright ___ORGANIZATIONNAME___ ___YEAR___. All rights reserved.
+//  Created by wangshengjin on 18/8/15.
+//  Copyright (c) 2018年 NBGeetest. All rights reserved.
 //
 
-#import "AppDelegate.h"
-#import "MainViewController.h"
-#import "NBGeetest.h";
+#import "AppDelegate+NBGeetest.h"
 #import <GT3Captcha/GT3Captcha.h>
-@interface AppDelegate ()<GT3CaptchaManagerDelegate>
-@end
+#import "NBGeetest.h"
 
-@implementation AppDelegate
-
-- (BOOL)application:(UIApplication*)application didFinishLaunchingWithOptions:(NSDictionary*)launchOptions
-{
-    self.viewController = [[MainViewController alloc] init];
-    return [super application:application didFinishLaunchingWithOptions:launchOptions];
-}
+@implementation AppDelegate (NBGeetest)
 
 #pragma mark GT3CaptchaManagerDelegate
 - (void)gtCaptcha:(GT3CaptchaManager *)manager errorHandler:(GT3Error *)error {
@@ -110,3 +82,4 @@
 }
 
 @end
+
